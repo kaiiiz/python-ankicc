7cfcb1b8e48cfa9fcbe426770d3d972f984db8a7
kaiiiz <ukaizheng@gmail.com>
2021-07-10 00:30:55 +0800

    Add test

--- ankicc/ankicc.py
+++ ankicc/ankicc.py
@@ -35,22 +35,22 @@ def main(args: Namespace):
 
     with zipfile.ZipFile(args.apkg_path, 'r') as zf:
         zf.extractall(collection_dir)
-        
+
     cc = opencc.OpenCC(args.convertor)
-    
+
     for col_name in TARGET_COLLECTION:
         col_path = os.path.join(collection_dir, col_name)
         if os.path.isfile(col_path):
             col = cc_collection(cc, Collection(col_path))
             col.write(modify=True, backup_folder=backup_dir)
-    
+   
     with zipfile.ZipFile(args.output_path, 'w', zipfile.ZIP_DEFLATED) as zf:
         for root, _, files in os.walk(collection_dir):
             for file_name in files:
                 zf.write(os.path.join(root, file_name), file_name)
-    
 
-def parse_args() -> Namespace:
+
+def parse_args(args: None) -> Namespace:
     parser = ArgumentParser()
     parser.add_argument(
         "--apkg_path", type=Path, required=True,
@@ -64,8 +64,11 @@ def parse_args() -> Namespace:
     parser.add_argument(
         "--convertor", choices=opencc.CONFIGS, default="s2t.json",
     )
-    
-    args = parser.parse_args()
+
+    if args:
+        args = parser.parse_args(args)
+    else:
+        args = parser.parse_args()
     return args
 
 
